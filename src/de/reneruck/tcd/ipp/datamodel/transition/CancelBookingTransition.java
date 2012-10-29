1b73c945765198d721936fbf6e038fc4c1c7fb77
Rene <reneruck@googlemail.com>
2012-10-29 17:42:55 +0000

    new class CancelBookingTransition

---
+++ src/de/reneruck/tcd/ipp/datamodel/transition/CancelBookingTransition.java
@@ -0,0 +1,68 @@
+package de.reneruck.tcd.ipp.datamodel.transition;
+
+import java.net.ConnectException;
+import java.util.Date;
+
+import de.reneruck.tcd.ipp.datamodel.Booking;
+import de.reneruck.tcd.ipp.datamodel.database.DatabaseConnection;
+
+public class CancelBookingTransition implements Transition {
+
+	private long transitionId;
+	private Booking booking;
+	private Date processingDate;
+	private TransitionState transitionState;
+	private String reason;
+
+	public CancelBookingTransition(Booking booking) {
+		this.booking = booking;
+		this.transitionId = booking.getId();
+	}
+
+	@Override
+	public long getTransitionId() {
+		return this.transitionId;
+	}
+
+	@Override
+	public Booking getBooking() {
+		return this.booking;
+	}
+
+	@Override
+	public Date getProcessingDate() {
+		return this.processingDate;
+	}
+
+	@Override
+	public TransitionState getTransitionState() {
+		return this.transitionState;
+	}
+
+	@Override
+	public String getReason() {
+		return this.reason;
+	}
+
+	@Override
+	public void performTransition(DatabaseConnection connection) throws ConnectException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHandlingDate(Date handlingDate) {
+		this.processingDate = handlingDate;
+	}
+
+	@Override
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	@Override
+	public void setTransitionState(TransitionState state) {
+		this.transitionState = state;
+	}
+
+}
